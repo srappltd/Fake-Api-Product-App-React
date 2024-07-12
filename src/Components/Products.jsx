b6657dce33f943969548f2e4102ecb2bbe8d2102
srappltd <abhaygautam626039@gmail.com>
2024-07-12 20:44:09 +0530

    filter solve

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -10,13 +10,14 @@ const Products = ({products}) => {
   const [filterMenu,setFilterMenu] = useState(false)
   const {search} = useLocation()
   const navigate = useNavigate(null)
+  console.log(search)
     const productCategory = productFilter.products.map(product=>{return product.category})
 
     const categories = productCategory.filter((category,index)=>productCategory.indexOf(category) === index)
   return (
     <div className='w-full md:w-[80%]'>
       <div className='w-full flex md:hidden px-3 flex-col items-end select-none'>
-       <div className='py-2 px-3 border mt-2 whitespace-nowrap text-xs flex items-center justify-center font-medium cursor-pointer' onClick={()=>setFilterMenu(!filterMenu)}>{ search != undefined ? decodeURIComponent(search.split('=')[1]) :  'Select Now'} <i class="ri-arrow-down-s-fill"></i></div>
+       <div className='py-2 px-3 border mt-2 whitespace-nowrap text-xs flex items-center justify-center font-medium cursor-pointer' onClick={()=>setFilterMenu(!filterMenu)}>{ search ? decodeURIComponent(search.split('=')[1]) :  'Select Now'} <i class="ri-arrow-down-s-fill"></i></div>
        <div className={`w-full mt-2 text-xs cursor-pointer border px-2 py-2 ${ filterMenu ? 'flex' : 'hidden'} transition-all duration-300 flex-col`}>
 {categories.length != 0 ? categories.map((category,index)=><Link to={`/filter?category=${category}`} className={`w-full py-2 ${'border-b'} font-medium`} onClick={()=>setFilterMenu(!filterMenu)}>{category}</Link>) : '' }
         
